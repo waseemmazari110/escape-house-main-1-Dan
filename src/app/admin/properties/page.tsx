c4190991a91200b9c1a47039bcc01207c120011a
waseemmazari110 <waseemmazari110@gmail.com>
2025-12-04 04:50:45 +0500

    Error fixed

--- src/app/admin/properties/page.tsx
+++ src/app/admin/properties/page.tsx
@@ -328,7 +328,7 @@ function AdminPropertiesPageContent() {
                     </Button>
                   </div>
                 </div>
-              )}}
+              )}
             </>
           )}
         </div>
